d2738d2b2410d3b758e020817665b1fe97ba6299
Ryan Campbell <veeneck@gmail.com>
2015-11-23 15:28:51 -0500

    Initial Commit

---
+++ Particleboard/Particleboard.h
@@ -0,0 +1,19 @@
+//
+//  Particleboard.h
+//  Particleboard
+//
+//  Created by Ryan Campbell on 11/23/15.
+//  Copyright © 2015 Phalanx Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Particleboard.
+FOUNDATION_EXPORT double ParticleboardVersionNumber;
+
+//! Project version string for Particleboard.
+FOUNDATION_EXPORT const unsigned char ParticleboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Particleboard/PublicHeader.h>
+
+
