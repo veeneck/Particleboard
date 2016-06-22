8dbeabdb5cf77ea3ea4a08930c56a346f298f380
Ryan Campbell <ryancampbell@Ryans-iMac-2.local>
2016-06-22 16:36:40 -0400

    Build for macOS

---
+++ ParticleboardOS/ParticleboardOS.h
@@ -0,0 +1,19 @@
+//
+//  ParticleboardOS.h
+//  ParticleboardOS
+//
+//  Created by Ryan Campbell on 6/22/16.
+//  Copyright © 2016 Phalanx Studios. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ParticleboardOS.
+FOUNDATION_EXPORT double ParticleboardOSVersionNumber;
+
+//! Project version string for ParticleboardOS.
+FOUNDATION_EXPORT const unsigned char ParticleboardOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParticleboardOS/PublicHeader.h>
+
+
